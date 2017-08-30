2be930d3fd562a378e2bc2d938e4c14f33f06484
louiskane <lkane02@yahoo.com>
2017-08-29 21:57:22 -0700

    adding second component

--- angular2first/app/courses.component.ts
+++ angular2first/app/courses.component.ts
@@ -2,9 +2,19 @@ import {Component} from 'angular2/core';
 
 @Component({
 	selector: 'courses'
-	template: '<h2>courses<h2>'
+	template: `
+		<h2>courses<h2>
+		{{ title }}
+		<ul>
+			<li *ngFor="#course of courses">
+				{{ course }}
+			<li>
+
+		</ul>
+		`
 })
 
 export class CoursesComponent {
-
+	title: string = "the title of courses page";
+	courses= ;
 }
